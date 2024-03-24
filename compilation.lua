ca3ee4b9c657c5cf84142c53fbdc80126f4c37b2
Kietveva <157573052+Kietveva@users.noreply.github.com>
2024-03-24 08:16:18 +0700

    Update compilation.lua

--- compilation.lua
+++ compilation.lua
@@ -139,11 +139,12 @@ Tab:AddLabel("Game Script")
 Tab:AddButton({
 	Name = "W azureV2 (Blox Fruit",
 	Callback = function()
-gfalse
-			v().Team = "Pirates"
+getgenv().Team = "Pirates"
 getgenv().FixCrash = false -- Turn it On For Hopping Server, Improve Performance But Silent Aim On Mob And Player
 getgenv().FixCrash2 = false -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+		end
+}) 
 
 Tab:AddButton({
 	Name = "sol's rng",
