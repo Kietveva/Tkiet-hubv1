522fbf0883c8e62ed78a908f9b650c1959174068
Kietveva <157573052+Kietveva@users.noreply.github.com>
2024-03-30 06:59:33 +0700

    Add files via upload

--- tkiet-hub.lua
+++ tkiet-hub.lua
@@ -1,32 +1,53 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Tkiet hub ", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-
-local Tab = Window:MakeTab({
-	Name = "Main 🏠",
-	Icon = "rbxassetid://16603971574",
-	PremiumOnly = false
-})
-Tab:AddLabel("Made by Tkiet")
-Tab:AddLabel("Is Not Auto")
-Tab:AddLabel("Need Pray or Gwa Gwa Spawn In Workspace")
-Tab:AddLabel("To The Tp Work")
-
-Tab:AddButton({
-	Name = "TP to Pray",
-	Callback = function()
-      		local player = game.Players.LocalPlayer local workspace = game:GetService("Workspace")
-
-for _,v in pairs(workspace:GetChildren()) do if v.Name == "Pray" then player.Character.HumanoidRootPart.CFrame = v.CFrame end end
-  	end    
-})
-
-Tab:AddButton({
-	Name = "TP to GwaGwa",
-	Callback = function()
-      		local player = game.Players.LocalPlayer local workspace = game:GetService("Workspace")
-
-for _,v in pairs(workspace:GetChildren()) do if v.Name == "GwaGwa" then player.Character.HumanoidRootPart.CFrame = v.CFrame end end
-  	end    
-})
-
-OrionLib:Init()
+NotificationLibrary:SendNotification("Warning", "Check Game...", 3)
+wait(2.89)
+NotificationLibrary:SendNotification("Info", "Injected...", 3)
+wait(2.95)
+NotificationLibrary:SendNotification("Success", "Script Load !", 3)
+wait(0.1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dqtixz/Script-no-scope-by-dqtixz-mb-and-pc/main/free"))();
+elseif game.PlaceId == 6722284015 then
+    local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
+wait(0.1)
+NotificationLibrary:SendNotification("Info", "Made By Tkiet x Yui KGT", 3)
+wait(2)
+NotificationLibrary:SendNotification("Warning", "Check Game...", 3)
+wait(2.89)
+NotificationLibrary:SendNotification("Info", "Injected...", 3)
+wait(2.95)
+NotificationLibrary:SendNotification("Success", "Script Load !", 3)
+wait(0.1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dqtixz/Pistol-1v1-script/main/Projeto%20lka"))();
+elseif game.PlaceId == 10549750305 then
+    local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
+wait(0.1)
+NotificationLibrary:SendNotification("Info", "Made By Tkiet x Yui KGT", 3)
+wait(2)
+NotificationLibrary:SendNotification("Warning", "Check Game...", 3)
+wait(2.89)
+NotificationLibrary:SendNotification("Info", "Injected...", 3)
+wait(2.95)
+NotificationLibrary:SendNotification("Success", "Script Load !", 3)
+wait(0.1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dqtixz/Free-Fire-Max-Roblox-Script-By-dqtixz-Open-Source/main/Free"))();
+elseif game.PlaceId == 1240123653 then
+    local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
+wait(0.1)
+NotificationLibrary:SendNotification("Info", "Made By Tkiet x Yui KGT", 3)
+wait(2)
+NotificationLibrary:SendNotification("Warning", "Check Game...", 3)
+wait(2.89)
+NotificationLibrary:SendNotification("Info", "Injected...", 3)
+wait(2.95)
+NotificationLibrary:SendNotification("Success", "Script Load !", 3)
+wait(0.1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dqtixz/Zombie-attack-New/main/Script%20by%20dqtixz%20full%20OP"))();
+else
+end
+--10549750305
+--anti afk kick
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
