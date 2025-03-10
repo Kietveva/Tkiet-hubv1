226bd45df346915f98d16783e1e9e55fa9c61752
Kietveva <157573052+Kietveva@users.noreply.github.com>
2025-03-10 18:22:20 +0700

    AluoiHub.lua

--- Tkiet hub.lua
+++ Tkiet hub.lua
@@ -3,8 +3,8 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 --------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
-    Title = "Tkiet Hub",
-    SubTitle = "by Tkiet",
+    Title = "A Lưới Hub 🔥🔥",
+    SubTitle = "by Kyet Kyet",
     TabWidth = 160,
     Size = UDim2.fromOffset(450, 300),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
